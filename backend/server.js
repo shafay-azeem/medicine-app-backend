2ea09b11a80fe811a68ab44ab200cabc9b1d0248
faizan-99 <mfaizy605@gmail.com>
2023-03-29 15:16:13 +0500

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -11,23 +11,25 @@ dotenv.config({ path: "backend/.env" });
 connectDataBase();
 
 //------Deployment-----------------------------------------------
-// const __dirname1 = path.resolve();
-// console.log(process.env.NODE_ENV, "process.env.NODE_ENV");
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname1, "/dine-in/build")));
-//     app.get("*", (req, res) =>
-//         res.sendFile(path.resolve(__dirname1, "dine-in", "build", "index.html"))
-//     );
-// } else {
-//     app.get("/", (req, res) => {
-//         res.send("API is running..");
-//     });
-// }
+const __dirname1 = path.resolve();
+console.log(process.env.NODE_ENV, "process.env.NODE_ENV");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/Medicine-App/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname1, "Medicine-App", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 //----------------------------------------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`server is running on ${process.env.PORT}`);
+  console.log(`server is running on ${process.env.PORT}`);
 });
